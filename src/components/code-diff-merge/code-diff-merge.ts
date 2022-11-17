f1af5a17256e1cb60581ea9d3afe04e43b1a71ea
feiya <yufei1313@gmail.com>
2022-11-18 01:32:37 +0800

    处理diff

--- src/components/code-diff-merge/code-diff-merge.ts
+++ src/components/code-diff-merge/code-diff-merge.ts
@@ -1,16 +1,33 @@
-import { diffLines } from 'diff'
+import { diffLines, Change } from 'diff'
 import { TextLine } from './text-line'
 
-export function getDiff(oldStr: string, newStr: string) {
-    var diffContent = diffLines(oldStr, newStr);
-    console.log(diffContent)
+export function getDiff(oldStr: string, newStr: string,): Array<TextLine[]> {
+    let changes: Change[] = diffLines(oldStr, newStr);
+    console.log(changes)
+    console.log(changes.length)
+
+    var result: Array<TextLine[]> = new Array();
+
+    if (!changes || changes.length == 0) {
+        return result;
+    }
 
     var oldLines: TextLine[] = new Array();
+    var newLines: TextLine[] = new Array();
+
+    result.push(oldLines);
+    result.push(newLines);
+
+    changes.forEach(e=>{
+        
+    })
 
     var line = new TextLine('sds', true, 1);
     line.add = true;
     oldLines.push(line);
     console.log(oldLines);
+
+    return result;
 }
 
 export default getDiff
